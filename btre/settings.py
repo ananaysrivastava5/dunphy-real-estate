d4d21bd7b43be001c393a33d9aec0a36681b7316
Ananay Srivastava <ananaysrivastava5@gmail.com>
2021-07-14 17:29:02 -0700

    Debug true

--- btre/settings.py
+++ btre/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '-#^1o!7z4jpx8%$0j+&tw6o#0!3=0a%p5rd=5$_h8$@d!47er@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['dunphyrealestate.herokuapp.com', '127.0.0.1']
 
@@ -38,7 +38,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
